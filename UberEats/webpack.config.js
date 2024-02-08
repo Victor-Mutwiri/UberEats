e0260eb589bc349edebfc800c12331963dde89a7
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-02-08 18:36:24 +0300

    1.5

---
+++ UberEats/webpack.config.js
@@ -0,0 +1,16 @@
+export default {
+    // Your other webpack configurations here...
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+      ],
+    },
+  };
+  
